a9a107bcfcee9669a62923a0879b00ec959784fb
Lionel Nicolas <lionel.nicolas@nividic.org>
2014-06-11 00:35:49 -0400

    switcher_ifconfig: add activate and deactivate implementation

--- plugins/switcher/ifconfig.py
+++ plugins/switcher/ifconfig.py
@@ -18,15 +18,77 @@ config_checks = {
 config_optional = [
 ]
 
+def execute(cmd, timeout=10):
+    try:
+        pipe = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    except Exception, e:
+        log.error("Failed to execute command '%s' (%s)", cmd, e)
+        return 1, ""
+
+    res   = pipe.poll()
+    start = time.time()
+
+    while res == None and time.time() - start < timeout:
+        res = pipe.poll()
+        time.sleep(0.1)
+
+    if res == None:
+        pipe.kill()
+        log.warn("Killed command '%s'" % (cmd))
+        res = 1
+
+    return res, pipe.stdout.readlines()
+
+def add_virtual_ip(interface, virtual_ip):
+    res, output = execute("ip addr add dev %s local %s" % (interface, virtual_ip))
+
+    if res:
+        log.error("failed to set virtual IP %s on %s", virtual_ip, interface)
+        return False
+
+    return True
+
+def del_virtual_ip(interface, virtual_ip):
+    res, output = execute("ip addr delete dev %s local %s" % (interface, virtual_ip))
+
+    if res:
+        log.error("failed to set virtual IP %s on %s", virtual_ip, interface)
+        return False
+
+    return True
+
+def send_gratuitous_arp(interface, virtual_ip):
+    res, output = execute("arping -U %s -c 2 -I %s" % (virtual_ip, interface))
+
+    if res:
+        log.error("failed to send gratuitous ARP on %s", interface)
+        return False
+
+    return True
 
 ### PLUGIN INTERFACE ###
 
 # function called by monitor to activate traffic
 def activate():
+    global interface
+    global virtual_ip
+
+    if not add_virtual_ip(interface, virtual_ip):
+        return False
+
+    if not send_gratuitous_arp(interface, virtual_ip):
+        return False
+
     return True
 
 # function called by monitor to activate traffic
 def deactivate():
+    global interface
+    global virtual_ip
+
+    if not del_virtual_ip(interface, virtual_ip):
+        return False
+
     return True
 
 ### END OF PLUGIN INTERFACE ###

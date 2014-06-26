19901b9b847ed991a57ddcc5f13c8e592ed8884c
Lionel Nicolas <lionel.nicolas@nividic.org>
2014-06-25 21:32:49 -0400

    notifier_mail: initial SMTP client implementation

--- plugins/notifier/mail.py
+++ plugins/notifier/mail.py
@@ -2,14 +2,75 @@
 # -*- coding: utf-8 -*-
 
 import config
+import email.mime.text
+import smtplib
 
+import mod_logger
 log = mod_logger.initlog(__name__)
 
+DEFAULT_PORT = {
+    "none": 25,
+    "ssl":  465,
+    "tls":  587,
+}
+
 config_checks = {
+    "server":     { "type": "str",  "default": "localhost",         "check": False },
+    "port":       { "type": "int",  "default": False,               "check": False },
+    "sender":     { "type": "str",  "default": "koublad@localhost", "check": False },
+    "recipients": { "type": "list", "default": False,               "check": "checkNonEmptyList(value)" },
+    "encryption": { "type": "str",  "default": "none",              "check": "in ['none', 'ssl', 'tls']" },
+    "username":   { "type": "str",  "default": False,               "check": False },
+    "password":   { "type": "str",  "default": False,               "check": False },
 }
 
 config_optional = [
+    "port",
+    "username",
+    "password",
 ]
+
+def send_email(subject, body):
+    global server
+    global port
+    global sender
+    global recipients
+    global encryption
+    global username
+    global password
+
+    if not port:
+        port = DEFAULT_PORT[encryption]
+
+    if   encryption in [ "none", "tls" ]:
+        smtpserver = smtplib.SMTP(server, port)
+    elif encryption in [ "ssl" ]:
+        smtpserver = smtplib.SMTP_SSL(server, port)
+    else:
+        return False
+
+    msg = email.mime.text.MIMEText(body)
+    msg.set_charset("utf-8")
+
+    msg["Subject"] = subject
+    msg["From"]    = sender
+    msg["To"]      = ', '.join(recipients)
+
+    smtpserver.set_debuglevel(0) # set to 1 if you want SMTP protocol debugging
+    smtpserver.ehlo()
+
+    if encryption == "tls":
+        smtpserver.starttls()
+        smtpserver.ehlo()
+
+    if username and password:
+        smtpserver.login(username, password)
+
+    smtpserver.sendmail(sender, recipients, msg.as_string())
+    smtpserver.quit()
+
+    return True
+
 
 ### PLUGIN INTERFACE ###
 
@@ -31,6 +92,15 @@ def event(msg):
 # read configuration
 config_dict = config.defaultVariables(config_checks)
 config_dict = config.parseConfigurationFile(config.config_file, config_checks, config_optional, config_dict, plugin_name=__name__)
+
+# set variable globaly for easier access
+server     = config_dict['server']
+port       = config_dict['port']
+sender     = config_dict['sender']
+recipients = config_dict['recipients']
+encryption = config_dict['encryption']
+username   = config_dict['username']
+password   = config_dict['password']
 
 log.info("Plugin '%s' loaded" % (__name__))
 config.show(config_dict)
